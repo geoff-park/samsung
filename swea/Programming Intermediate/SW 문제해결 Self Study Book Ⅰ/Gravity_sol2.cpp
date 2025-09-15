c5e3297e7b43febd76b61d5f943b5845a398b96a
geoff-park <geoffrey.youngha.park@gmail.com>
2025-09-15 10:38:28 +0900

    Create Gravity_sol2.cpp

---
+++ swea/Programming Intermediate/SW 문제해결 Self Study Book Ⅰ/Gravity_sol2.cpp
@@ -0,0 +1,73 @@
+// Gravity
+// Solution 2: O(n^2)
+// 가장 높은 상자만 낙차 계산
+
+#include <bits/stdc++.h>
+
+#define EMPTY 0
+#define BOX 1
+
+using namespace std;
+
+int i, j, k;
+int testCase;
+int T;
+int room[100][100];
+int roomWidth, roomHeight;
+int boxHeight;
+int maxFallen;
+int countEmptySpace;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> testCase;
+
+  for (int T = 1; T <= testCase; ++T) {
+    cin >> roomWidth;
+    roomHeight = 100;
+
+    // init
+    maxFallen = 0;
+    for (int i = 0; i < roomWidth; ++i) {     // 90도 회전하기 때문에 roomWidth를 세로로 생각
+      for (int j = 0; j < roomHeight; ++j) {  // 마찬가지로 roomHeight를 가로로 생각
+        room[i][j] = EMPTY;
+      }
+    }
+
+    for (int i = 0; i < roomWidth; ++i) {
+      cin >> boxHeight;
+      for (int j = 0; j < boxHeight; ++j) {
+        room[i][j] = BOX;
+      }
+    }
+
+    // 각 상자의 낙차를 구한다: O(n^2)
+    for (int i = 0; i < roomWidth; ++i) {
+      int flag = 0;
+
+      for (int j = 0; j < roomHeight; ++j) {
+        // room[i][j]가 박스일 때 빈칸의 개수를 셈으로써 낙차를 구한다.
+        if (room[i][j] == BOX) {
+          flag = 1;
+
+          countEmptySpace = 0;
+          for (int k = i + 1; k < roomWidth; ++k) {
+            if (room[k][j] == EMPTY) countEmptySpace++;
+          }
+
+          // 이전에 구한 낙차보다 현재 낙차가 크다면 업데이트한다.
+          if (countEmptySpace > maxFallen) maxFallen = countEmptySpace;
+        }
+      }
+
+      if (flag) break;
+    }
+
+    cout << "#" << T << ' ';
+    cout << maxFallen << '\n';
+  }
+
+  return 0;
+}
